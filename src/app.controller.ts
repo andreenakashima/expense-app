1fc41100a7b600180244a8dd106ba1c3974d9168
andreenakashima <andreenakashima@gmail.com>
2023-07-30 15:57:07 -0300

    Get all reports

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { ReportType, data } from 'src/data';
 
-@Controller('report/income')
+@Controller('report/:type')
 export class AppController {
   @Get()
-  getAllReports() {
-    return [];
+  getAllReports(@Param('type') type: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+
+    return data.report.filter((report) => report.type === reportType);
   }
 
   @Get(':id')
--- src/data.ts
+++ src/data.ts
@@ -9,11 +9,36 @@ interface DataProps {
   }[];
 }
 
-enum ReportType {
+export enum ReportType {
   INCOME = 'income',
   EXPENSE = 'expense',
 }
 
 export const data: DataProps = {
-  report: [],
+  report: [
+    {
+      id: 'uuid1',
+      source: 'Salary',
+      amount: 7500,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.INCOME,
+    },
+    {
+      id: 'uuid2',
+      source: 'YouTube',
+      amount: 2500,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.INCOME,
+    },
+    {
+      id: 'uuid3',
+      source: 'Food',
+      amount: 500,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.EXPENSE,
+    },
+  ],
 };
