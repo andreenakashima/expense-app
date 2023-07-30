8e552bb52b3a7a128f6172169535060d3b4b3bbb
andreenakashima <andreenakashima@gmail.com>
2023-07-30 15:41:54 -0300

    Create data file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('report/income')
 export class AppController {
   @Get()
-  getAllIncomeReports() {
+  getAllReports() {
     return [];
+  }
+
+  @Get(':id')
+  getReportById() {
+    return {};
+  }
+
+  @Post()
+  createReport() {
+    return 'Created';
+  }
+
+  @Put(':id')
+  updateReport() {
+    return 'updated';
+  }
+
+  @Delete(':id')
+  deleteReport() {
+    return 'deleted';
   }
 }
---
+++ src/data.ts
@@ -0,0 +1,19 @@
+interface DataProps {
+  report: {
+    id: string;
+    source: string;
+    amount: number;
+    created_at: Date;
+    updated_at: Date;
+    type: ReportType;
+  }[];
+}
+
+enum ReportType {
+  INCOME = 'income',
+  EXPENSE = 'expense',
+}
+
+export const data: DataProps = {
+  report: [],
+};
